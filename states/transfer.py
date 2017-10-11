66cd15c8ba9bf473a3798f3e80bcea1671ac490a
zake7749 <zake7749@gmail.com>
2017-10-11 11:05:26 +0800

    Add state-meta for state tracking.

---
+++ states/transfer.py
@@ -0,0 +1,13 @@
+class State(object):
+
+    def __init__(self):
+        pass
+
+    def restore(self, record):
+        pass
+
+
+class StateMachine(object):
+
+    def __init__(self):
+        pass
